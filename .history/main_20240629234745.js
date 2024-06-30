97333c509dcd912ed694db49f44732837b31eb53
paragonmac <paragonmac@gmail.com>
2024-06-30 05:51:07 -0400

    Add .history to .gitignore

--- .history/main_20240629234745.js
+++
@@ -1,9 +0,0 @@
-import { loadGameData } from './gameDataLoader.js';
-import { initializeGame } from './gameInitializer.js';
-
-// Start loading game data
-loadGameData().then(data => {
-    initializeGame(data);
-}).catch(error => {
-    console.error('Failed to start the game:', error);
-});
